fad9c43fc75332fd70e00fc8f50b00a2a29cf25f
aymanFayez <aymanfayez277@gmail.com>
2020-04-07 19:34:00 +0200

    Logger interface is added

---
+++ IB-Logger/Interfaces/LoggerProtocol.swift
@@ -0,0 +1,18 @@
+//
+//  LoggerProtocol.swift
+//  IB-Logger
+//
+//  Created by A.F on 4/7/20.
+//  Copyright © 2020 A.F. All rights reserved.
+//
+
+import Foundation
+
+public protocol LoggerProtocol {
+    func log(message: String, level: LogLevel)
+    func fetch() -> String
+}
+
+public enum LogLevel {
+    
+}
