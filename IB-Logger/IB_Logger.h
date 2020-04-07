dadfb97d978eadcf4290acaa91793a3f94ee41af
aymanFayez <aymanfayez277@gmail.com>
2020-04-07 19:25:00 +0200

    Initial Commit

---
+++ IB-Logger/IB_Logger.h
@@ -0,0 +1,19 @@
+//
+//  IB_Logger.h
+//  IB-Logger
+//
+//  Created by A.F on 4/7/20.
+//  Copyright © 2020 A.F. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IB_Logger.
+FOUNDATION_EXPORT double IB_LoggerVersionNumber;
+
+//! Project version string for IB_Logger.
+FOUNDATION_EXPORT const unsigned char IB_LoggerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IB_Logger/PublicHeader.h>
+
+
